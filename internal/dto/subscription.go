dc3a7b3cb3e065ed0c23740d5a881aef333b2f66
theun1c <belovdima1122@yandex.ru>
2026-05-01 20:42:15 +0300

    feat: add dtos and validation

---
+++ internal/dto/subscription.go
@@ -0,0 +1,30 @@
+package dto
+
+type CreateSubscriptionRequest struct {
+	ServiceName string  `json:"service_name"`
+	Price       int     `json:"price"`
+	UserID      string  `json:"user_id"`
+	StartDate   string  `json:"start_date"`
+	EndDate     *string `json:"end_date,omitempty"`
+}
+
+type UpdateSubscriptionRequest struct {
+	ServiceName string  `json:"service_name"`
+	Price       int     `json:"price"`
+	UserID      string  `json:"user_id"`
+	StartDate   string  `json:"start_date"`
+	EndDate     *string `json:"end_date,omitempty"`
+}
+
+type SubscriptionResponse struct {
+	ID          string  `json:"id"`
+	ServiceName string  `json:"service_name"`
+	Price       int     `json:"price"`
+	UserID      string  `json:"user_id"`
+	StartDate   string  `json:"start_date"`
+	EndDate     *string `json:"end_date,omitempty"`
+}
+
+type SubscriptionListResponse struct {
+	Subscriptions []SubscriptionResponse `json:"subscriptions"`
+}
